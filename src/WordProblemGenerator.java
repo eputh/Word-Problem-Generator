d353562d8e986d3e5fd125a5425b9c0682fbb086
Emily <emily.puth@gmail.com>
2016-02-11 21:45:05 -0800

    Map in preparation for Java chart library

--- src/WordProblemGenerator.java
+++ src/WordProblemGenerator.java
@@ -4,42 +4,45 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Emily on 2/11/2016.
  */
 public class WordProblemGenerator {
 
-    private ArrayList<Float> scoreRecord;
+    private Map<Integer, Float> scoreRecord;
 
     public WordProblemGenerator() {
-        scoreRecord = new ArrayList<Float>();
+        scoreRecord = new HashMap<Integer, Float>();
     }
 
-    private void addScoreToRecord(Float score) {
-        scoreRecord.add(score);
+    private void addScoreToRecord(Integer worksheetID, Float score) {
+        scoreRecord.put(worksheetID, score);
     }
 
 
     public static void main(String args[]) {
         WordProblemGenerator generator = new WordProblemGenerator();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        int i = 0;
         boolean anotherWorksheet = true;
         while (anotherWorksheet) {
             Worksheet worksheet = new Worksheet();
-            generator.addScoreToRecord(worksheet.getScore());
+            generator.addScoreToRecord(i, worksheet.getScore());
             System.out.println("\nWould you like to generate another worksheet? Y/N");
             try {
                 String input = br.readLine();
                 input = input.replace(" ", "");
                 if (!input.equals("Y")) anotherWorksheet = false;
+                else i++;
             }
             catch (IOException e) {
                 System.err.println(e);
             }
         }
-        for (Float score : generator.scoreRecord) System.out.println(score);
+        for (Map.Entry<Integer, Float> entry : generator.scoreRecord.entrySet()) System.out.println(entry.getKey() + ": " + entry.getValue());
     }
 
 }
